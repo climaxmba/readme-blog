c56f2588ceb208ea9043d4ad0fb1143c1a9756ea
Climax Mba <climaxium150@gmail.com>
2024-09-10 15:41:41 +0100

    Install raw-loader for markdown

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader"
+    })
+
+    return config
+  }
 }
