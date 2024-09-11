b88aba64bb1af4f72f848e4c75506a02f9c23656
Climax Mba <climaxium150@gmail.com>
2024-09-11 11:27:11 +0100

    Add useDebounce & search box component

---
+++ src/components/searchBox/SearchBox.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { MenuItem, Select, TextField } from "@mui/material";
+import { SetStateAction } from "react";
+
+import styles from "./searchBox.module.scss";
+
+interface SearchAndFilterProps {
+  query: string;
+  setQuery: React.Dispatch<SetStateAction<string>>;
+  category: string;
+  setCategrory: React.Dispatch<SetStateAction<string>>;
+  categories: string[];
+}
+
+export default function SearchBox({
+  query,
+  setQuery,
+  category,
+  setCategrory,
+  categories,
+}: SearchAndFilterProps) {
+  return (
+    <div className={styles.container}>
+      <TextField
+        variant="filled"
+        label="Search Blogs"
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
+        sx={{ ".MuiInputBase-root": { bgcolor: "transparent" } }}
+      />
+
+      <Select
+        label="Categories"
+        variant="filled"
+        defaultValue={category}
+        onChange={(e) => setCategrory(e.target.value)}
+        sx={{
+          minWidth: 100,
+        }}
+      >
+        <MenuItem value="0" defaultChecked>
+          <i>All Categories</i>
+        </MenuItem>
+        {categories.map((val) => (
+          <MenuItem key={val} value={val}>
+            {val}
+          </MenuItem>
+        ))}
+      </Select>
+    </div>
+  );
+}
